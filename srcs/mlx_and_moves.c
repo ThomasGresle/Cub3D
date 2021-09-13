d39f735904a6d36d9758f7b91c016a74bb86317e
ThomasGresle <50533608+ThomasGresle@users.noreply.github.com>
2021-09-13 21:24:11 +0200

    Update mlx_and_moves.c

--- srcs/mlx_and_moves.c
+++ srcs/mlx_and_moves.c
@@ -1,6 +1,6 @@
 #include "../includes/cub3d.h"
 
-int		release_keyboard(int keycode, t_list *config)
+int	release_keyboard(int keycode, t_list *config)
 {
 	if (keycode == FORWARD)
 		config->data.forward = 0;
@@ -17,7 +17,7 @@ int		release_keyboard(int keycode, t_list *config)
 	return (1);
 }
 
-int		press_keyboard(int keycode, t_list *config)
+int	press_keyboard(int keycode, t_list *config)
 {
 	if (keycode == FORWARD)
 		config->data.forward = 1;
@@ -51,7 +51,8 @@ void	switch_images(t_list *config)
 void	init_mlx(t_list *config, char **map)
 {
 	set_player(config);
-	if (!(config->data.mlx_ptr = mlx_init()))
+	config->data.mlx_ptr = mlx_init();
+	if (!(config->data.mlx_ptr))
 	{
 		printf("Error\nLa mlx ne se lance pas");
 		free_str_and_map(config, map);
@@ -61,13 +62,18 @@ void	init_mlx(t_list *config, char **map)
 	get_and_check_texture(config);
 	if (config->texerror == 0)
 	{
-		config->data.img = mlx_new_image(config->data.mlx_ptr, config->width, config->height);
-		config->data.addr = (int *)mlx_get_data_addr(config->data.img, &config->data.bits_per_pixel,
-							&config->data.line_length, &config->data.endian);
-		config->data.mlx_win = mlx_new_window(config->data.mlx_ptr, config->width, config->height, "Raycasting cub3d");
-		config->data.img2 = mlx_new_image(config->data.mlx_ptr, config->width, config->height);
-		config->data.addr2 = (int *)mlx_get_data_addr(config->data.img2, &config->data.bits_per_pixel,
-							&config->data.line_length, &config->data.endian);
+		config->data.img = mlx_new_image(config->data.mlx_ptr,
+			config->width, config->height);
+		config->data.addr = (int *)mlx_get_data_addr(config->data.img,
+			&config->data.bits_per_pixel,
+			&config->data.line_length, &config->data.endian);
+		config->data.mlx_win = mlx_new_window(config->data.mlx_ptr,
+			config->width, config->height, "Raycasting cub3d");
+		config->data.img2 = mlx_new_image(config->data.mlx_ptr,
+			config->width, config->height);
+		config->data.addr2 = (int *)mlx_get_data_addr(config->data.img2,
+			&config->data.bits_per_pixel,
+			&config->data.line_length, &config->data.endian);
 		mlx_hook(config->data.mlx_win, 33, 1L << 17, clear_mlx, config);
 		mlx_hook(config->data.mlx_win, 2, 1L << 0, press_keyboard, config);
 		mlx_hook(config->data.mlx_win, 3, 1L << 1, release_keyboard, config);
