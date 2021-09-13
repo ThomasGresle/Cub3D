80a27977406ef7460e47c12eba375e6af2430945
ThomasGresle <50533608+ThomasGresle@users.noreply.github.com>
2021-09-13 23:35:06 +0200

    Update text_utils.c

--- srcs/text_utils.c
+++ srcs/text_utils.c
@@ -1,37 +1,43 @@
 #include "../includes/cub3d.h"
 
+void	get_and_check_texture2(t_list *config)
+{
+	if (!(config->texture[2].img = mlx_xpm_file_to_image(config->data.mlx_ptr,
+		config->west_t, &(config->texture[2].width),
+		&(config->texture[2].height))))
+	{
+		printf("Error\nTexture ouest incorrecte\n");
+		config->texerror = 1;
+	}
+	else if (!(config->texture[3].img = mlx_xpm_file_to_image(config->data.mlx_ptr,
+		config->east_t, &(config->texture[3].width),
+		&(config->texture[3].height))))
+	{
+		printf("Error\nTexture est incorrecte\n");
+		config->texerror = 1;
+	}
+	else 
+		init_texture_address(config);
+}
+
 void	get_and_check_texture(t_list *config)
 {
 	if (!(config->texture[0].img = mlx_xpm_file_to_image(config->data.mlx_ptr,
 		config->north_t, &(config->texture[0].width),
 		&(config->texture[0].height))))
-		{
-			printf("Error\nTexture nord incorrecte\n");
-			config->texerror = 1;
-		}
+	{
+		printf("Error\nTexture nord incorrecte\n");
+		config->texerror = 1;
+	}
 	else if (!(config->texture[1].img = mlx_xpm_file_to_image(config->data.mlx_ptr,
 		config->south_t, &(config->texture[1].width),
 		&(config->texture[1].height))))
-		{
-			printf("Error\nTexture sud incorrecte\n");
-			config->texerror = 1;
-		}
-	else if (!(config->texture[2].img = mlx_xpm_file_to_image(config->data.mlx_ptr,
-		config->west_t, &(config->texture[2].width),
-		&(config->texture[2].height))))
-		{
-			printf("Error\nTexture ouest incorrecte\n");
-			config->texerror = 1;
-		}
-	else if (!(config->texture[3].img = mlx_xpm_file_to_image(config->data.mlx_ptr,
-		config->east_t, &(config->texture[3].width),
-		&(config->texture[3].height))))
-		{
-			printf("Error\nTexture est incorrecte\n");
-			config->texerror = 1;
-		}
-	else 
-		init_texture_address(config);
+	{
+		printf("Error\nTexture sud incorrecte\n");
+		config->texerror = 1;
+	}
+	else
+		get_and_check_texture2(config);
 }
 
 void	choice_texture(t_list *config, char **map)
