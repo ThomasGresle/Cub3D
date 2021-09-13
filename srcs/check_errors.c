e95f3e8f6a340fba2bc7c5e7449c6790b2529650
ThomasGresle <50533608+ThomasGresle@users.noreply.github.com>
2021-09-13 19:40:27 +0200

    Update check_errors.c

--- srcs/check_errors.c
+++ srcs/check_errors.c
@@ -1,23 +1,23 @@
 #include "../includes/cub3d.h"
 
-int     ft_check_argv(char **argv)
+int	ft_check_argv(char **argv)
 {
-    int i;
+    int	i;
 
     i = 0;
-    if (argv[1][0] == '.' || argv[1][0] == '\0')
-        return (0);
-    while (argv[1][i] && argv[1][i] != '.')
-        i++;
-    if (argv[1][i] == '\0')
-        return (0);
-    i++;
-    if (ft_strcmp(argv[1], "cub", i))
-        return (1);
-    return (0);
+	if (argv[1][0] == '.' || argv[1][0] == '\0')
+		return (0);
+	while (argv[1][i] && argv[1][i] != '.')
+		i++;
+	if (argv[1][i] == '\0')
+		return (0);
+	i++;
+	if (ft_strcmp(argv[1], "cub", i))
+		return (1);
+	return (0);
 }
 
-int		ft_check_args(int argc, char **argv)
+int	ft_check_args(int argc, char **argv)
 {
     if (argc != 2)
 	{
@@ -32,11 +32,11 @@ int		ft_check_args(int argc, char **argv)
 	return (1);
 }
 
-int		ft_check_config(char *str)
+int	ft_check_config(char *str)
 {
-	int i;
-	int j;
-	int number;
+	int	i;
+	int	j;
+	int	number;
 
 	i = 0;
 	j = 0;
@@ -54,11 +54,11 @@ int		ft_check_config(char *str)
 	return (1);
 }
 
-int		ft_check_map(char **line, t_list *config)
+int	ft_check_map(char **line, t_list *config)
 {
-	int i;
-	int j;
-	
+	int	i;
+	int	j;
+
 	i = config->startx;
 	j = config->starty;
 	if (line_nbr(line, &(*config)) == 0)
@@ -70,13 +70,13 @@ int		ft_check_map(char **line, t_list *config)
 	return (config->error);
 }
 
-int		check_and_parse(int argc, char **argv, char ***map, t_list *config)
+int	check_and_parse(int argc, char **argv, char ***map, t_list *config)
 {
 	if (ft_check_args(argc, argv) == 0)
 		return (0);
 	if (ft_check_file(argv[1]) == 0)
 		return (0);
-    ft_parse_file(argv, &(*map), &(*config));
+	ft_parse_file(argv, &(*map), &(*config));
 	if (ft_check_config(config->ceiling_color) == 0)
 		return (0);
 	config->final_ceiling = final_color(config, config->ceiling_color);
