5a788d6654cffd7821cc0b90ea134fd715f98e44
ThomasGresle <50533608+ThomasGresle@users.noreply.github.com>
2021-09-13 20:10:59 +0200

    Update cub3d.c

--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -1,11 +1,11 @@
 #include "../includes/cub3d.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_list	config;
+	t_list	config;
 	int		i;
 
-    config.map = NULL;
+	config.map = NULL;
 	i = 0;
 	struct_init(&config);
 	if (check_and_parse(argc, argv, &config.map, &config) == 0)
@@ -15,5 +15,5 @@ int main(int argc, char **argv)
 	}
 	init_mlx(&config, config.map);
 	free_str_and_map(&config, config.map);
-    return (0);
+	return (0);
 }
