975bcc14f54a2ac96dae55bdae82b3d5eb5dbbf8
ThomasGresle <50533608+ThomasGresle@users.noreply.github.com>
2021-09-13 21:26:52 +0200

    Update mlx_and_moves.c

--- srcs/mlx_and_moves.c
+++ srcs/mlx_and_moves.c
@@ -48,6 +48,15 @@ void	switch_images(t_list *config)
 	config->data.addr2 = tmp;
 }
 
+void	mlx_on(t_list *config)
+{
+	mlx_hook(config->data.mlx_win, 33, 1L << 17, clear_mlx, config);
+	mlx_hook(config->data.mlx_win, 2, 1L << 0, press_keyboard, config);
+	mlx_hook(config->data.mlx_win, 3, 1L << 1, release_keyboard, config);
+	mlx_loop_hook(config->data.mlx_ptr, final_raycasting, config);
+	mlx_loop(config->data.mlx_ptr);
+}
+
 void	init_mlx(t_list *config, char **map)
 {
 	set_player(config);
@@ -63,21 +72,17 @@ void	init_mlx(t_list *config, char **map)
 	if (config->texerror == 0)
 	{
 		config->data.img = mlx_new_image(config->data.mlx_ptr,
-			config->width, config->height);
+				config->width, config->height);
 		config->data.addr = (int *)mlx_get_data_addr(config->data.img,
-			&config->data.bits_per_pixel,
+				&config->data.bits_per_pixel,
 			&config->data.line_length, &config->data.endian);
 		config->data.mlx_win = mlx_new_window(config->data.mlx_ptr,
-			config->width, config->height, "Raycasting cub3d");
+				config->width, config->height, "Raycasting cub3d");
 		config->data.img2 = mlx_new_image(config->data.mlx_ptr,
-			config->width, config->height);
+				config->width, config->height);
 		config->data.addr2 = (int *)mlx_get_data_addr(config->data.img2,
-			&config->data.bits_per_pixel,
+				&config->data.bits_per_pixel,
 			&config->data.line_length, &config->data.endian);
-		mlx_hook(config->data.mlx_win, 33, 1L << 17, clear_mlx, config);
-		mlx_hook(config->data.mlx_win, 2, 1L << 0, press_keyboard, config);
-		mlx_hook(config->data.mlx_win, 3, 1L << 1, release_keyboard, config);
-		mlx_loop_hook(config->data.mlx_ptr, final_raycasting, config);
-		mlx_loop(config->data.mlx_ptr);
+		mlx_on(config);
 	}
 }
