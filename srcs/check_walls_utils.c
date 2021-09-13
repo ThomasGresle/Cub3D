cb7089d367fd8a599836c640414ecd86aecf3cae
ThomasGresle <50533608+ThomasGresle@users.noreply.github.com>
2021-09-13 20:09:11 +0200

    Update check_walls_utils.c

--- srcs/check_walls_utils.c
+++ srcs/check_walls_utils.c
@@ -2,14 +2,19 @@
 
 void	check_right(char **map, int *i, int *j, t_list *config)
 {
-	if (*i == 0 || *j == 0 || *i >= config->line_nbr || *j >= config->column_nbr)
+	if (*i == 0 || *j == 0 || *i >= config->line_nbr
+		|| *j >= config->column_nbr)
 		error_msg(*i, *j, &(*config));
-	while (config->error == 0 && map[(*i)][(*j)] && map[(*i) + 1][(*j)] && map[(*i) + 1][(*j)] != '1' && map[(*i)][(*j) + 1] && map[(*i)][(*j) + 1] == '1')
+	while (config->error == 0 && map[(*i)][(*j)]
+			&& map[(*i) + 1][(*j)] && map[(*i) + 1][(*j)] != '1'
+			&& map[(*i)][(*j) + 1] && map[(*i)][(*j) + 1] == '1')
 	{
 		(*i)++;
 		if (*i >= config->line_nbr || map[(*i) + 1][(*j)] == ' ')
 			error_msg(*i, *j, &(*config));
-		if (config->error == 0 && map[(*i)][(*j)] && map[(*i)][(*j) + 1] && map[(*i)][(*j) + 1] != '1')
+		if (config->error == 0 && map[(*i)][(*j)]
+			&& map[(*i)][(*j) + 1]
+			&& map[(*i)][(*j) + 1] != '1')
 		{
 			(*j)++;
 			check_top(map, i, j, &(*config));
@@ -19,14 +24,19 @@ void	check_right(char **map, int *i, int *j, t_list *config)
 
 void	check_top(char **map, int *i, int *j, t_list *config)
 {
-	if (*i == 0 || *j == 0 || *i >= config->line_nbr || *j >= config->column_nbr)
+	if (*i == 0 || *j == 0 || *i >= config->line_nbr
+		|| *j >= config->column_nbr)
 		error_msg(*i, *j, &(*config));
-	while (config->error == 0 && map[(*i)][(*j)] && map[(*i)][(*j) + 1] && map[(*i)][(*j) + 1] != '1' && map[(*i) - 1][(*j)] && map[(*i) - 1][(*j)] == '1')
+	while (config->error == 0 && map[(*i)][(*j)]
+			&& map[(*i)][(*j) + 1] && map[(*i)][(*j) + 1] != '1'
+			&& map[(*i) - 1][(*j)] && map[(*i) - 1][(*j)] == '1')
 	{
 		(*j)++;
 		if (*j >= config->column_nbr || map[(*i)][(*j) + 1] == ' ')
 			error_msg(*i, *j, &(*config));
-		if (config->error == 0 && map[(*i)][(*j)] && map[(*i) - 1][(*j)] && map[(*i) - 1][(*j)] != '1')
+		if (config->error == 0 && map[(*i)][(*j)]
+			&& map[(*i) - 1][(*j)]
+			&& map[(*i) - 1][(*j)] != '1')
 		{
 			(*i)--;
 			check_left(map, i, j, &(*config));
@@ -36,14 +46,19 @@ void	check_top(char **map, int *i, int *j, t_list *config)
 
 void	check_left(char **map, int *i, int *j, t_list *config)
 {
-	if (*i == 0 || *j == 0 || *i >= config->line_nbr || *j >= config->column_nbr)
+	if (*i == 0 || *j == 0 || *i >= config->line_nbr
+		|| *j >= config->column_nbr)
 		error_msg(*i, *j, &(*config));
-	while (config->error == 0 && map[(*i)][(*j)] && map[(*i) - 1][(*j)] && map[(*i) - 1][(*j)] != '1' && map[(*i)][(*j) - 1] && map[(*i)][(*j) - 1] == '1')
+	while (config->error == 0 && map[(*i)][(*j)] && map[(*i) - 1][(*j)]
+			&& map[(*i) - 1][(*j)] != '1' && map[(*i)][(*j) - 1]
+			&& map[(*i)][(*j) - 1] == '1')
 	{
 		(*i)--;
 		if (*i == 0 || map[(*i) - 1][(*j)] == ' ')
 			error_msg(*i, *j, &(*config));
-		if (config->error == 0 && map[(*i)][(*j)] && map[(*i)][(*j) - 1] && map[(*i)][(*j) - 1] != '1')
+		if (config->error == 0 && map[(*i)][(*j)]
+			&& map[(*i)][(*j) - 1]
+			&& map[(*i)][(*j) - 1] != '1')
 		{
 			(*j)--;
 			check_bottom(map, i, j, &(*config));
@@ -53,14 +68,18 @@ void	check_left(char **map, int *i, int *j, t_list *config)
 
 void	check_bottom(char **map, int *i, int *j, t_list *config)
 {
-	if (*i == 0 || *j == 0 || *i >= config->line_nbr || *j >= config->column_nbr)
+	if (*i == 0 || *j == 0 || *i >= config->line_nbr
+		|| *j >= config->column_nbr)
 		error_msg(*i, *j, &(*config));
-	while (config->error == 0 && map[(*i)][(*j)] && map[(*i)][(*j) - 1] && map[(*i)][(*j) - 1] != '1' && map[(*i) + 1][(*j)] && map[(*i) + 1][(*j)] == '1')
+	while (config->error == 0 && map[(*i)][(*j)] && map[(*i)][(*j) - 1]
+			&& map[(*i)][(*j) - 1] != '1' && map[(*i) + 1][(*j)]
+			&& map[(*i) + 1][(*j)] == '1')
 	{
 		(*j)--;
 		if (*j == 0 || map[(*i)][(*j) - 1] == ' ')
 			error_msg(*i, *j, &(*config));
-		if (config->error == 0 && map[(*i)][(*j)] && map[(*i) + 1][(*j)] && map[(*i) + 1][(*j)] != '1')
+		if (config->error == 0 && map[(*i)][(*j)]
+			&& map[(*i) + 1][(*j)] && map[(*i) + 1][(*j)] != '1')
 		{
 			(*i)++;
 			check_right(map, i, j, &(*config));
