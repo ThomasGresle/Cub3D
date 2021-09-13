081e31c83b60331bc1b0e7bb865b73c34bcee14a
ThomasGresle <50533608+ThomasGresle@users.noreply.github.com>
2021-09-13 23:14:41 +0200

    Update dda.c

--- srcs/dda.c
+++ srcs/dda.c
@@ -5,17 +5,17 @@ void	check_hit(t_list *config, char **map)
 	while (config->hit == 0)
 	{
 		if (config->sidedistx < config->sidedisty)
-        {
+		{
 			config->sidedistx += config->deltadistx;
 			config->mapx += config->stepx;
 			config->side = 0;
-        }
-        else
-        {
+		}
+		else
+		{
 			config->sidedisty += config->deltadisty;
 			config->mapy += config->stepy;
 			config->side = 1;
-        }
+		}
 		if (map[config->mapx][config->mapy] == '1') 
 			config->hit = 1;
 	}
@@ -24,11 +24,11 @@ void	check_hit(t_list *config, char **map)
 void	distance_and_lines(t_list *config, char **map)
 {
 	if (config->side == 0)
-		config->perpwalldist = (config->mapx - config->posx +
-		(1 - config->stepx) / 2) / config->raydirx;
+		config->perpwalldist = (config->mapx - config->posx
+			+ (1 - config->stepx) / 2) / config->raydirx;
 	else
-		config->perpwalldist = (config->mapy - config->posy +
-		(1 - config->stepy) / 2) / config->raydiry;
+		config->perpwalldist = (config->mapy - config->posy
+			+ (1 - config->stepy) / 2) / config->raydiry;
 	config->lineheight = (int)(config->height / config->perpwalldist);
 	config->drawstart = -config->lineheight / 2 + config->height / 2;
 	if (config->drawstart < 0)
@@ -44,16 +44,17 @@ void	init_draw(t_list *config, char **map)
 	int	j;
 
 	config->drawend = config->height - config->drawstart;
-
 	i = config->drawend;
 	j = -1;
 	while (++j < config->drawstart)
-		config->data.addr[j * config->data.line_length / 4 + config->x] = config->final_ceiling;
+		config->data.addr[j * config->data.line_length / 4
+			+ config->x] = config->final_ceiling;
 	if (j <= config->drawend)
 		draw_text(config, map, config->x, j);
 	j = i;
 	while (++j < config->height)
-		config->data.addr[j * config->data.line_length / 4 + config->x] = config->final_floor;
+		config->data.addr[j * config->data.line_length / 4
+			+ config->x] = config->final_floor;
 }
 
 void	draw_text(t_list *config, char **map, int x, int y)
@@ -62,14 +63,17 @@ void	draw_text(t_list *config, char **map, int x, int y)
 	choice_texture(config, map);
 	config->step = 1.0 * config->texture[0].height / config->lineheight;
 	texture_calculation(config, map);
-	config->texpos = (config->drawstart - config->height / 2 +
-		config->lineheight / 2) * config->step;
+	config->texpos = (config->drawstart - config->height / 2
+		+ config->lineheight / 2) * config->step;
 	while (++y <= config->drawend)
 	{
-		config->texy = (int)config->texpos & (config->texture[config->texnum].height - 1);
+		config->texy = (int)config->texpos
+			& (config->texture[config->texnum].height - 1);
 		config->texpos += config->step;
 		if (y < config->height && x < config->width)
 			config->data.addr[y * config->data.line_length / 4 + x] = 
-			config->texture[config->texnum].addr[config->texy * config->texture[config->texnum].line_length / 4 + config->texx];
+			config->texture[config->texnum].addr[config->texy
+				* config->texture[config->texnum].line_length / 4
+				+ config->texx];
 	}
 }
