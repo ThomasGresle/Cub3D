ace1765520bac72aa78ee534b0776e5946c6a7a2
ThomasGresle <50533608+ThomasGresle@users.noreply.github.com>
2021-09-13 23:38:48 +0200

    Update text_utils.c

--- srcs/text_utils.c
+++ srcs/text_utils.c
@@ -2,42 +2,44 @@
 
 void	get_and_check_texture2(t_list *config)
 {
-	if (!(config->texture[2].img = mlx_xpm_file_to_image(config->data.mlx_ptr,
+	config->texture[2].img = mlx_xpm_file_to_image(config->data.mlx_ptr,
 		config->west_t, &(config->texture[2].width),
-		&(config->texture[2].height))))
+		&(config->texture[2].height));
+	if (!(config->texture[2].img))
 	{
 		printf("Error\nTexture ouest incorrecte\n");
 		config->texerror = 1;
 	}
-	else if (!(config->texture[3].img = mlx_xpm_file_to_image(config->data.mlx_ptr,
+	config->texture[3].img = mlx_xpm_file_to_image(config->data.mlx_ptr,
 		config->east_t, &(config->texture[3].width),
-		&(config->texture[3].height))))
+		&(config->texture[3].height));
+	if (!(config->texture[3].img))
 	{
 		printf("Error\nTexture est incorrecte\n");
 		config->texerror = 1;
 	}
-	else 
-		init_texture_address(config);
+	init_texture_address(config);
 }
 
 void	get_and_check_texture(t_list *config)
 {
-	if (!(config->texture[0].img = mlx_xpm_file_to_image(config->data.mlx_ptr,
+	config->texture[0].img = mlx_xpm_file_to_image(config->data.mlx_ptr,
 		config->north_t, &(config->texture[0].width),
-		&(config->texture[0].height))))
+		&(config->texture[0].height));
+	if (!(config->texture[0].img))
 	{
 		printf("Error\nTexture nord incorrecte\n");
 		config->texerror = 1;
 	}
-	else if (!(config->texture[1].img = mlx_xpm_file_to_image(config->data.mlx_ptr,
+	config->texture[1].img = mlx_xpm_file_to_image(config->data.mlx_ptr,
 		config->south_t, &(config->texture[1].width),
-		&(config->texture[1].height))))
+		&(config->texture[1].height));
+	if (!(config->texture[1].img))
 	{
 		printf("Error\nTexture sud incorrecte\n");
 		config->texerror = 1;
 	}
-	else
-		get_and_check_texture2(config);
+	get_and_check_texture2(config);
 }
 
 void	choice_texture(t_list *config, char **map)
@@ -59,7 +61,8 @@ void	texture_calculation(t_list *config, char **map)
 	else
 		config->wallx = config->posx + config->perpwalldist * config->raydirx;
 	config->wallx -= floor(config->wallx);
-	config->texx = (int)(config->wallx * (double)config->texture[config->texnum].width);
+	config->texx = (int)(config->wallx
+		* (double)config->texture[config->texnum].width);
 	if (config->side == 0 && config->raydirx > 0)
 		config->texx = config->texture[config->texnum].width - config->texx - 1;
 	if (config->side == 1 && config->raydiry < 0)
