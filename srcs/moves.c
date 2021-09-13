ceb66c002835b277fbdb6d934269a1b7e4cd419b
ThomasGresle <50533608+ThomasGresle@users.noreply.github.com>
2021-09-13 22:00:47 +0200

    Update moves.c

--- srcs/moves.c
+++ srcs/moves.c
@@ -4,16 +4,20 @@ void	move_forward_or_back(t_list *config, char **map)
 {
 	if (config->data.forward == 1)
 	{
-		if (map[(int)(config->posx + (config->dirx * config->movespeed * 2))][(int)config->posy] == '0')
+		if (map[(int)(config->posx + (config->dirx * config->movespeed
+			* 2))][(int)config->posy] == '0')
 			config->posx += config->dirx * config->movespeed;
-		if (map[(int)(config->posx)][(int)(config->posy + (config->diry * config->movespeed * 2))] == '0')
+		if (map[(int)(config->posx)][(int)(config->posy +
+			(config->diry * config->movespeed * 2))] == '0')
 			config->posy += config->diry * config->movespeed;
 	}
 	if (config->data.back == 1)
 	{
-		if (map[(int)(config->posx - (config->dirx * config->movespeed * 2))][(int)(config->posy)] == '0')
+		if (map[(int)(config->posx - (config->dirx * config->movespeed * 2))]
+			[(int)(config->posy)] == '0')
 			config->posx -= config->dirx * config->movespeed;
-		if (map[(int)(config->posx)][(int)(config->posy - (config->diry * config->movespeed * 2))] == '0')
+		if (map[(int)(config->posx)][(int)(config->posy
+			- (config->diry * config->movespeed * 2))] == '0')
 			config->posy -= config->diry * config->movespeed;
 	}
 }
@@ -22,16 +26,20 @@ void	move_right_or_left(t_list *config, char **map)
 {
 	if (config->data.right == 1)
 	{
-		if (map[(int)(config->posx + config->diry * (config->movespeed * 2))][(int)config->posy] == '0')
+		if (map[(int)(config->posx + config->diry *
+			(config->movespeed * 2))][(int)config->posy] == '0')
 			config->posx += config->diry * config->movespeed;
-		if (map[(int)config->posx][(int)(config->posy - config->dirx * (config->movespeed * 2))] == '0')
+		if (map[(int)config->posx][(int)(config->posy -
+			config->dirx * (config->movespeed * 2))] == '0')
 			config->posy -= config->dirx * config->movespeed;
 	}
 	if (config->data.left == 1)
 	{
-		if (map[(int)(config->posx - config->diry * (config->movespeed * 2))][(int)config->posy] == '0')
+		if (map[(int)(config->posx - config->diry *
+			(config->movespeed * 2))][(int)config->posy] == '0')
 			config->posx -= config->diry * config->movespeed;
-		if (map[(int)config->posx][(int)(config->posy + config->dirx * (config->movespeed * 2))] == '0')
+		if (map[(int)config->posx][(int)(config->posy + config->dirx
+			* (config->movespeed * 2))] == '0')
 			config->posy += config->dirx * config->movespeed;
 	}
 }
@@ -45,10 +53,14 @@ void	rotation_right(t_list *config)
 	olddirx = config->dirx;
 	if (config->data.rotate_right == 1)
 	{
-		config->dirx = config->dirx * cos(-config->rotspeed / 2) - config->diry * sin(-config->rotspeed / 2);
-		config->diry = olddirx * sin(-config->rotspeed / 2) + config->diry * cos(-config->rotspeed / 2);
-		config->planex = config->planex * cos(-config->rotspeed / 2) - config->planey * sin(-config->rotspeed / 2);
-		config->planey = oldplanex * sin(-config->rotspeed / 2) + config->planey * cos(-config->rotspeed / 2);
+		config->dirx = config->dirx * cos(-config->rotspeed / 2)
+			- config->diry * sin(-config->rotspeed / 2);
+		config->diry = olddirx * sin(-config->rotspeed / 2)
+			+ config->diry * cos(-config->rotspeed / 2);
+		config->planex = config->planex * cos(-config->rotspeed / 2)
+			- config->planey * sin(-config->rotspeed / 2);
+		config->planey = oldplanex * sin(-config->rotspeed / 2)
+			+ config->planey * cos(-config->rotspeed / 2);
 	}
 }
 
@@ -61,9 +73,13 @@ void	rotation_left(t_list *config)
 	olddirx = config->dirx;
 	if (config->data.rotate_left == 1)
 	{
-		config->dirx = config->dirx * cos(config->rotspeed / 2) - config->diry * sin(config->rotspeed / 2);
-		config->diry = olddirx * sin(config->rotspeed / 2) + config->diry * cos(config->rotspeed / 2);
-		config->planex = config->planex * cos(config->rotspeed / 2) - config->planey * sin(config->rotspeed / 2);
-		config->planey = oldplanex * sin(config->rotspeed / 2) + config->planey * cos(config->rotspeed / 2);
+		config->dirx = config->dirx * cos(config->rotspeed / 2)
+			- config->diry * sin(config->rotspeed / 2);
+		config->diry = olddirx * sin(config->rotspeed / 2)
+			+ config->diry * cos(config->rotspeed / 2);
+		config->planex = config->planex * cos(config->rotspeed / 2)
+			- config->planey * sin(config->rotspeed / 2);
+		config->planey = oldplanex * sin(config->rotspeed / 2)
+			+ config->planey * cos(config->rotspeed / 2);
 	}
 }
