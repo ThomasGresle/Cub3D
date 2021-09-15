1a632e7433db0ddcd7d445c2175943cf91627d64
ThomasGresle <50533608+ThomasGresle@users.noreply.github.com>
2021-09-15 19:56:28 +0200

    Update set_player.c

--- srcs/set_player.c
+++ srcs/set_player.c
@@ -6,7 +6,7 @@
 /*   By: tgresle <tgresle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 19:14:26 by tgresle           #+#    #+#             */
-/*   Updated: 2021/09/15 19:53:24 by tgresle          ###   ########.fr       */
+/*   Updated: 2021/09/15 19:56:20 by tgresle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	struct_init2(t_list *config)
 	config->mapx = (int)config->posx;
 	config->mapy = (int)config->posy;
 	config->movespeed = 0.05;
-	config->rotspeed = 0.05;
+	config->rotspeed = 0.06;
 	if (config->raydiry == 0)
 		config->deltadistx = 0;
 	else if (config->raydirx == 0)
